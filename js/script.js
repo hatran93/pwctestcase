ad99f60be71844536f7fd1b00bba77ab261bf2d0
hatran93 <hacektran@gmail.com>
2016-11-29 11:02:45 +0100

    edit responzive, added about-us.html and responsibility.html

--- js/script.js
+++ js/script.js
@@ -7,5 +7,29 @@ $(document).ready(function(){
     });
 
 
+    $("button#more-details").on('click', function() {
 
-});
+        if($("#more-details").hasClass('active')) {
+            $(".info-content-right").slideUp();
+            $("button#more-details").html("MORE DETAILS");
+            $("#more-details").removeClass('active');
+        }
+
+        else {
+            $(".info-content-right").slideDown();
+            $("button#more-details").html("LESS DETAILS");
+            $("#more-details").addClass('active');
+        }
+
+
+    });
+
+
+
+
+
+
+    });
+
+
+
